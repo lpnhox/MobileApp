e0eba34eac414044bc0a6e44e78009a6bd8538dd
31191026916 <92772504+31191026916@users.noreply.github.com>
2022-04-21 16:31:16 +0700

    Xong phan dang nhap - dang ky -reset

--- app/src/main/java/vu/htr/cs/muzikapp/login/ForgotPassword.java
+++ app/src/main/java/vu/htr/cs/muzikapp/login/ForgotPassword.java
@@ -1,21 +1,64 @@
 package vu.htr.cs.muzikapp.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 import vu.htr.cs.muzikapp.R;
 public class ForgotPassword extends AppCompatActivity {
+    TextInputEditText et_email;
+    Button button_send;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-    }
-    private void back_from_forgot(View v){
+        et_email=findViewById(R.id.et_email);
+        button_send= findViewById(R.id.button_send);
+
+        auth = FirebaseAuth.getInstance();
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=et_email.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(ForgotPassword.this, "Nhập địa chỉ email của bạn", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(ForgotPassword.this, "Email không hợp lệ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ForgotPassword.this, "Chúng tôi đã gửi email khôi phục cho bạn", Toast.LENGTH_SHORT).show();
+                            et_email.setText("");
+                        } else {
+                            Toast.makeText(ForgotPassword.this, "Email chưa được đăng ký!", Toast.LENGTH_SHORT).show();
+                            et_email.setText("");
+                        }
+                    }
+                });
 
 
+            }
+        });
     }
+
 
 }
--- app/src/main/java/vu/htr/cs/muzikapp/login/LoginActivity.java
+++ app/src/main/java/vu/htr/cs/muzikapp/login/LoginActivity.java
@@ -1,16 +1,32 @@
 package vu.htr.cs.muzikapp.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import vu.htr.cs.muzikapp.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    Button button_forgot_password,button_signup;
+    Button button_forgot_password,button_signup,button_login;
+    TextInputEditText et_username;
+    EditText et_password;
+    FirebaseAuth auth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +34,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(vu.htr.cs.muzikapp.R.layout.activity_login);
         button_signup= findViewById(R.id.button_signup);
         button_forgot_password = findViewById(R.id.button_forgot_password);
+        button_login=findViewById(R.id.button_login);
+        et_username=findViewById(R.id.et_username);
+        et_password=findViewById(R.id.et_password);
+
         button_signup.setOnClickListener(this);
         button_forgot_password.setOnClickListener(this);
+        button_login.setOnClickListener(this);
 
+        auth = FirebaseAuth.getInstance();
     }
 
 
@@ -33,6 +55,40 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         else if(view==button_forgot_password){
             Intent forgotIntent = new Intent(LoginActivity.this,ForgotPassword.class);
             startActivity(forgotIntent);
+        }
+        else if(view==button_login){
+            String email = et_username.getText().toString().trim();
+            String password= et_password.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(LoginActivity.this, "Nhập username/email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(getApplicationContext(), "Nhập Email hợp lệ", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(LoginActivity.this, "Nhập password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        if (password.length() < 6) {
+                            et_password.setError(getString(R.string.minimum_password));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại", Toast.LENGTH_LONG).show();
+                        }
+                        return;
+                    } else {
+                        Intent intent = new Intent(LoginActivity.this, ForgotPassword.class);
+                        startActivity(intent);
+                    }
+                }
+            });
 
         }
     }

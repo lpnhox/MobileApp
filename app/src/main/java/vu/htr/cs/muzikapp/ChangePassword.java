3c1c0dd5a7cbfc518cd8c5897babd907d8588fe3
VoThanhTai1109 <89510523+VoThanhTai1109@users.noreply.github.com>
2022-04-24 18:52:09 +0700

    add trang doi mat khau'

---
+++ app/src/main/java/vu/htr/cs/muzikapp/ChangePassword.java
@@ -0,0 +1,14 @@
+package vu.htr.cs.muzikapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
